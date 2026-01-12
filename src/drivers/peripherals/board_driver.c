ca873b2098e3e3a0244bd5d4d5481b76da388eae
NoHaxxJustLags <56199686+NoHaxxJustLags@users.noreply.github.com>
2026-01-12 19:22:04 +0100

    Hardware timers

--- src/drivers/peripherals/board_driver.c
+++ src/drivers/peripherals/board_driver.c
@@ -82,7 +82,6 @@ int board_driver_scan(uint64_t *board_state)
             return ret;
         }
 
-    /* Allow signals to settle before reading columns (increase if needed for stable reads) */
     k_sleep(K_USEC(1000));
 
         for (int col = 0; col < BOARD_COLS; col++) {
